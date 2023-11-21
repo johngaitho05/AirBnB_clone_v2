530c7c38fb3b7d047e7efe8f704cd0ec7a2435e0
John Gaitho <johngaitho05@gmail.com>
2023-11-21 15:51:29 +0300

    refactor

--- models/user.py
+++ models/user.py
@@ -3,15 +3,32 @@
 from sqlalchemy import Column, String
 
 from models.base_model import BaseModel
+from . import storage_type
 from .engine.db_storage import Base
 
+if storage_type == 'db':
+    class User(BaseModel, Base):
+        """
+        User ORM
+        """
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
 
-class User(BaseModel, Base):
-    """
-    User ORM
-    """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+else:
+    class User(BaseModel):
+        """Represent a User.
+
+        Public class attributes:
+            email (str): The email of the user.
+            password (str): The password of the user.
+            first_name (str): The first name of the user.
+            last_name (str): The last name of the user.
+        """
+
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
