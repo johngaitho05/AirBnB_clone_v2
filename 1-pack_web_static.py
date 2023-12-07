e0fcc4e35f2a225fce2fe8bb3acfdc9dec640209
John Gaitho <johngaitho05@gmail.com>
2023-12-07 14:13:54 +0300

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This script zips the content of web_static"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+# Define global variables/constants
+LOCAL_PATH = 'web_static'
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+
+    # Create the versions folder if it doesn't exist
+    local('mkdir -p versions')
+
+    # Get the current date and time
+    now = datetime.utcnow()
+    date_format = "%Y%m%d%H%M%S"
+    date_str = now.strftime(date_format)
+
+    # Define the archive path
+    archive_name = f"web_static_{date_str}.tgz"
+    archive_path = os.path.join("versions", archive_name)
+    # Print the log
+    print(f"Packing {LOCAL_PATH} to {archive_path}")
+
+    # Create the archive
+    local(f'tar -czvf {archive_path} {LOCAL_PATH}')
+
+    # Print the size of the archive
+    archive_size = os.path.getsize(archive_path)
+    print(f"{LOCAL_PATH} packed: {archive_path} -> {archive_size} Bytes")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    # Run the do_pack function when executing the script
+    do_pack()
--- fabfile.py
+++ fabfile.py
@@ -1,25 +1,2 @@
-from fabric import Connection
-
-
-def copy_script():
-    # Replace 'your_local_path' with the local path to your script
-    local_path = '/home/yk/Desktop/ALX/AirBnB_clone_v2/0-setup_web_static.sh'
-
-    # Replace 'your_remote_path' with the remote path on the server
-    remote_path = '/home/ubuntu/0-setup_web_static.sh'
-
-    # Replace 'your_username' with the SSH username for the remote server
-    username = 'ubuntu'
-
-    # Remote server
-    hosts = ['52.23.178.146', '100.26.223.57']
-
-    for host in hosts:
-        # Connect to the remote server
-        with Connection(host=host, user=username) as c:
-            # Upload the script to the remote server
-            c.put(local_path, remote_path)
-
-
-if __name__ == "__main__":
-    copy_script()
+def hello(who="world"):
+    print("Hello {who}!".format(who=who))
