17353a261ef3fa023e5a94813fe81254536addf4
John Gaitho <johngaitho05@gmail.com>
2023-12-07 16:02:41 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
 """This script deploys the static files to 2 remote servers"""
-import os
+from fabric.api import *
+from os.path import exists
 
-from fabric.api import env, put, run, sudo
 
 env.hosts = ['52.23.178.146', '100.26.223.57']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-
-    # Check if the archive exists
-    if not os.path.exists(archive_path):
-        print(f"Error: Archive {archive_path} not found.")
+    """
+    deploying an archive to a server
+    """
+    if not exists(archive_path):
         return False
-
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/")
-
-    # Extract the archive to /data/web_static/releases/<archive_filename_without_extension>
-    archive_filename = os.path.basename(archive_path)
-    archive_filename_without_extension = os.path.splitext(archive_filename)[0]
-    remote_path = f"/data/web_static/releases/{archive_filename_without_extension}"
-
-    run(f'mkdir -p {remote_path}')
-    run(f'tar -xzf /tmp/{archive_filename} -C {remote_path} --strip-components=1')
-
-    # Delete the archive from the web server
-    sudo(f'rm /tmp/{archive_filename}')
-
-    # Delete the symbolic link /data/web_static/current
-    run('rm -f /data/web_static/current')
-
-    # Create a new symbolic link to the new version
-    sudo(f'ln -s {remote_path} /data/web_static/current')
-
-    print("New version deployed successfully.")
-    return True
-
-
-if __name__ == '__main__':
-    do_deploy('versions/web_static_20231207111310.tgz')
+    try:
+        archive_fname = archive_path.split('/')[-1]
+        archive_bname = archive_fname.split('.')[0]
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_bname))
+        run('tar -xzf /tmp/{0}.tgz -C /data/web_static/releases/{0}'.format(
+                    archive_bname))
+        run(('mv /data/web_static/releases/{0}/web_static/* ' +
+            '/data/web_static/releases/{0}/').format(archive_bname))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_bname))
+        run('rm -f /tmp/{}'.format(archive_fname))
+        run('rm -f /data/web_static/current')
+        run(('ln -s /data/web_static/releases/{}' +
+            ' /data/web_static/current').format(archive_bname))
+        return True
+    except Exception:
+        return False
