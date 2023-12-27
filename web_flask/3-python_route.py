cf01c2deeae611d824a4a030ca5bb1cd1e6e54c0
John Gaitho <johngaitho05@gmail.com>
2023-12-27 12:12:49 +0300

    multi routes view

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,5 +20,11 @@ def c_something(text):
     return "C {}".format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_something(text='is cool'):
+    return "Python {}".format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run()
