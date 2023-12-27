02a2da8796640a71a30e9a96eec6f3ab657feb52
John Gaitho <johngaitho05@gmail.com>
2023-12-27 12:06:08 +0300

    dynamic urls

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def hbnb():
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def csomething(text):
-    return "C {}".format(text)
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
