94bcb6eb47b97e5b35fc671f1b2ea9ad44d90580
John Gaitho <johngaitho05@gmail.com>
2023-12-29 12:24:43 +0300

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,11 +16,10 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_view():
     """Renders a list of states"""
-    storage.reload()
     states = storage.all(State)
     return render_template('7-states_list.html',
                            states=states.values())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
