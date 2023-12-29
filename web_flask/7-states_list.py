0ddca64fb647ee760f5be698142ed8d4341655dd
John Gaitho <johngaitho05@gmail.com>
2023-12-29 11:55:10 +0300

    flask with sql

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A simple flask server that uses sqlachemy"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(request):
+    storage.close()
+
+
+@app.route('/states_list')
+def states_view():
+    states = storage.all(State)
+    print("states", [v.name for k, v in states.items()])
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run()
