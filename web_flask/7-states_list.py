d255efe0b6629406674a6133d4eb2ca5ae928ba4
John Gaitho <johngaitho05@gmail.com>
2023-12-29 12:07:51 +0300

    polish

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(request):
+def teardown_db(exception):
+    """Closes a db session"""
     storage.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_view():
+    """Renders a list of states"""
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('7-states_list.html', states=states.values())
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
