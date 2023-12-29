fd223eaa5ae69828f8a4173253b32419a02b3164
John Gaitho <johngaitho05@gmail.com>
2023-12-29 14:39:42 +0300

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,11 +13,11 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_view():
     """Renders a list of states"""
     states = storage.all(State)
-    return render_template('7-states_list.html',
+    return render_template('8-cities_by_states.html',
                            states=states.values())
 
 
