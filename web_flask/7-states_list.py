55dd85d7469fd7b909acaa00fd6d41f3aae6be18
John Gaitho <johngaitho05@gmail.com>
2023-12-29 12:26:14 +0300

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A simple flask server that uses sqlachemy"""
+"""A simple flask server that uses SQLAlchemy"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
