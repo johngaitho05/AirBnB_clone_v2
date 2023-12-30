730b0650012f52fd35d96e31c47d3e369137e77a
John Gaitho <johngaitho05@gmail.com>
2023-12-30 11:00:42 +0300

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A simple flask server with a list view and a detail view of states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes a db session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_view(id=None):
+    """Renders a list of cities grouped by states"""
+    if not id:
+        states = storage.all(State)
+        return render_template('9-states.html',
+                               states=states.values())
+    state_id = 'State.{}'.format(id)
+    state = storage.all(State).get(state_id)
+    return render_template('9-states.html',
+                           state=state)
+
+
+if __name__ == "__main__":
+    app.run()
